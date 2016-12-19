6ece24d9425c0296c8cf198b4465fe8392fdfcf9
Hendrik Mans <hendrik@mans.de>
2016-12-19 14:20:33 +0100

    Add the beginnings of an editing action

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -17,6 +17,12 @@ defmodule Oldskool.PostController do
     render(conn, "show.html", post: post)
   end
 
+  def edit(conn, %{"id" => id}) do
+    # TODO: check if current_user is allowed to edit the post!
+    post = conn |> current_site_posts |> Repo.get(id)
+    render(conn, "edit.html", post: post)
+  end
+
   defp current_site_posts(conn) do
      assoc(conn.assigns.current_site, :posts)
   end
