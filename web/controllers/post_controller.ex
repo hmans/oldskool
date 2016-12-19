a148550ac5a06f9b74d6e0129cf1af190e8c813e
Hendrik Mans <hendrik@mans.de>
2016-12-19 14:47:36 +0100

    Edit posts

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -1,7 +1,7 @@
 defmodule Oldskool.PostController do
   use Oldskool.Web, :controller
 
-  # alias Oldskool.Post
+  alias Oldskool.Post
 
   def index(conn, _) do
     posts = conn
@@ -20,7 +20,20 @@ defmodule Oldskool.PostController do
   def edit(conn, %{"id" => id}) do
     # TODO: check if current_user is allowed to edit the post!
     post = conn |> current_site_posts |> Repo.get(id)
-    render(conn, "edit.html", post: post)
+    changeset = Post.changeset(post)
+    render(conn, "edit.html", post: post, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "post" => post_params}) do
+    post = conn |> current_site_posts |> Repo.get(id)
+    changeset = Post.changeset(post, post_params)
+
+    case Repo.update(changeset) do
+      {:ok, _} ->
+        redirect(conn, to: post_path(conn, :show, post))
+      {:error, changeset} ->
+        render(conn, "edit.html", post: post, changeset: changeset)
+    end
   end
 
   defp current_site_posts(conn) do
