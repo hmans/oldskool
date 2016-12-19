0095ed7729215f9c991718c98abd1e40281ec387
Hendrik Mans <hendrik@mans.de>
2016-12-19 18:02:25 +0100

    Add Turbolinks for speeeeed!

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -13,6 +13,11 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+// Turbolinks
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
+
+
 // Import local files
 //
 // Local files can be imported directly using relative
